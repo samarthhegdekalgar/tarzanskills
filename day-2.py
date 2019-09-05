b1816d62b7922913e38dce935a679188b6ae6e4b
Samartha Hegde <samarthhegdekalgar@gmail.com>
2019-09-05 13:38:57 +0530

    updated with comment

--- day-2.py
+++ day-2.py
@@ -1,23 +1,23 @@
-print("I will now count my chickens:")
+print("I will now count my chickens:")  #I will now count my chickens:
 
-print("Hens", 25 + 30 / 6)
-print("Roosters", 100 - 25 * 3 % 4)
+print("Hens", 25 + 30 / 6)      #30
+print("Roosters", 100 - 25 * 3 % 4)     #Roosters 97
 
-print("Now i will count the eggs:")
+print("Now i will count the eggs:")     #Now i will count the eggs:
 
-print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)        #6.75
 
-print("Is it true that 3 + 2 < 5 - 7?")
+print("Is it true that 3 + 2 < 5 - 7?")     #Is it true that 3 + 2 < 5 - 7?
 
-print(3 + 2 < 5 - 7)
+print(3 + 2 < 5 - 7)    #False
 
-print("What is 3 + 2?", 3 + 2)
-print("what is 5 - 7?", 5 - 7)
+print("What is 3 + 2?", 3 + 2)  #What is 3 + 2? 5
+print("what is 5 - 7?", 5 - 7)  #what is 5 - 7? -2
 
-print("oh, that's why it's False.")
+print("oh, that's why it's False.")     #oh, that's why it's False.
 
-print("How about some more.")
+print("How about some more.")       #How about some more.
 
-print("Is it greater?", 5 > -2)
-print("Is it greater or equal?", 5 >= -2)
-print("Is it less or equal?", 5 <= -2)
+print("Is it greater?", 5 > -2)     #Is it greater? True
+print("Is it greater or equal?", 5 >= -2)   #Is it greater or equal? True
+print("Is it less or equal?", 5 <= -2)  #Is it less or equal False
